00f2a7013126972a727f6c4baa1cdf05fbfef2d1
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2026-01-04 01:56:55 -0600

    version funcional

--- app/api/db/config.js
+++ app/api/db/config.js
@@ -1,5 +1,7 @@
 import pg from "pg";
 
+console.log("Database URL:", process.env.DATABASE_URL);
+
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
--- app/api/db/route.js
+++
@@ -1,6 +0,0 @@
-import DashboardController from "@/app/api/controllers/DashboardController";
-
-export async function GET(request) {
-    const controller = new DashboardController();
-    return await controller.getRecentActivity(request);
-}
